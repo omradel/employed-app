31c7d102561c1223d75ba231ed4ebcc6426e535a
omr <omradelbakry375@gmail.com>
2025-05-18 12:46:42 +0300

    fix error on deployment

--- src/components/recomJops.vue
+++ src/components/recomJops.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import JobCard from "./JobCard.vue";
 import { useJops } from "@/compasables/jobsLogic";
 import { Skeleton } from "@/components/ui/skeleton";
