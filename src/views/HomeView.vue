b6cacee75593ee4a240e35b24633668f16d8a282
omr <omradelbakry375@gmail.com>
2025-05-18 11:08:33 +0300

    add skeleton loader to jops list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import JobCard from '@/components/JobCard.vue'
 import MultiApplyCard from '@/components/MultiApplyCard.vue'
 import { useJops } from '@/compasables/jobsLogic'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const { jobs, loading, error } = useJops()
 
@@ -11,7 +12,7 @@ const { jobs, loading, error } = useJops()
   <div class="max-w-7xl mx-auto py-6 px-4">
     <h1 class="text-2xl font-bold">Explore New Career Opportunities</h1>
     <div class="grid grid-cols-12 gap-4 mt-6">
-      <div class="jobs-list-card  border-2 xl:col-span-8 lg:col-span-8 md:col-span-12 col-span-12">
+      <div class="jobs-list-card  border-2 xl:col-span-8 lg:col-span-8 md:col-span-12 col-span-12 bg-white">
         <div class="card-header pt-0 pb-0 bg-secondary flex items-center">
           <div class=" flex gap-x-2 items-center py-4 px-3 bg-white cursor-pointer border-r">
             <img src="@/assets/images/star.svg" alt="star" class="w-5 h-5">
@@ -19,8 +20,21 @@ const { jobs, loading, error } = useJops()
           </div>
           <span class="text-sm font-medium py-3 px-3 cursor-pointer">Outside Egypt</span>
         </div>
+
+        <div v-if="loading"  class="flex justify-between p-4 border-b" v-for="sketon  in 5" :key="sketon">
+          <div class="flex flex-col gap-2 w-full">
+            <Skeleton class="h-6 w-3/4"/>
+            <Skeleton class="h-6 w-1/2  "/>
+            <div class="flex flex-col gap-2 mt-5">
+              <Skeleton class="h-6 w-3/4"/>
+              <Skeleton class="h-6 w-3/4"/>
+              <Skeleton class="h-6 w-1/2"/>
+            </div>
+          </div>
+          <Skeleton class="h-[4rem] w-[4rem]"/>
+        </div>
         
-        <JobCard v-for="(job, index) in jobs" :key="index" v-bind="job" :class="index !== jobs.length - 1 ? 'border-b' : ''"  />
+        <JobCard v-else v-for="(job, index) in jobs" :key="index" v-bind="job" :class="index !== jobs.length - 1 ? 'border-b' : ''"  />
       </div>
       
       <div class="xl:col-span-4 lg:col-span-4 md:col-span-12 col-span-12 space-y-4">
